9478506e49a06d8faac795e907925bb63580cd77
hogeun9x <hogeun_9x@naver.com>
2021-04-15 16:46:01 +0900

    CommonController - goIndex() 생성 [수정 필요]

--- src/main/java/com/essam/www/common/CommonController.java
+++ src/main/java/com/essam/www/common/CommonController.java
@@ -2,12 +2,17 @@ package com.essam.www.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CommonController {
 	@Autowired
 	private CommonMM comm;
-//	메인페이지로 이동	
+//	메인페이지로 이동
+	@RequestMapping(value = "/")
+	String goIndex() {
+		return "home"; // .jsp
+	}
 //	new/hot 클래스목록 가져오기	
 //	검색페이지 이동	
 //	검색 결과 가져오기(ajax)	
