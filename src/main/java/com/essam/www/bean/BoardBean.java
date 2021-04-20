097577932a1a9e67db1ab58d4bd81443939b49f1
hogeun9x <hogeun_9x@naver.com>
2021-04-20 10:41:11 +0900

    bean - BoardBean 추가

---
+++ src/main/java/com/essam/www/bean/BoardBean.java
@@ -0,0 +1,23 @@
+package com.essam.www.bean;
+
+import java.util.List;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Alias("boardbean")
+@Data
+@Accessors(chain = true)
+public class BoardBean {
+	private String clsBrdNo; // 게시글번호
+	private String clsNo; // 클래스번호
+	private int clsBrdType; // 
+	private String mbId; // 작성자아이디
+	private String mbNickName; // 작성자닉네임
+	private String clsBrdTitle; // 제목
+	private String clsBrdContent; // 본문
+	private String clsBrdDate; // 작성일
+	private List<FileBean> files; // 첨부파일 목록
+}
