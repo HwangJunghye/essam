b70aef1838a32c511c1779b6a157f1c82d55568f
JUNGHYEHWANG <lovely_opal@naver.com>
2021-04-20 10:51:40 +0900

    admin.jsp페이지 이동 구현/ views 폴더에 하위 폴더 생성(class, etc)

--- src/main/java/com/essam/www/etc/EtcController.java
+++ src/main/java/com/essam/www/etc/EtcController.java
@@ -1,12 +1,35 @@
 package com.essam.www.etc;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.essam.www.bean.MemberBean;
 
 @Controller
 public class EtcController {
 	@Autowired
 	private EtcMM em;
-//	관리자 페이지 이동	
-//	통계 가져오기(ajax)	
+		// 관리자 페이지 이동
+		@RequestMapping(value = "/admin")
+		ModelAndView goAdmin(HttpSession session) {
+			ModelAndView mav = em.goAdmin(session);
+			return mav;
+		}
+				
+		// 통계 가져오기(ajax)	
+		//@RequestMapping(value = "/getstatistic")
+		//String getStatistic() {
+		// return null;	
+		//}
+
 }
--- src/main/java/com/essam/www/etc/EtcMM.java
+++ src/main/java/com/essam/www/etc/EtcMM.java
@@ -1,12 +1,31 @@
 package com.essam.www.etc;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.essam.www.bean.ClassBean;
 
 @Service
 public class EtcMM {
 	@Autowired
 	private IEtcDao eDao;
-//	관리자 페이지 이동	
-//	통계 가져오기(ajax)	
+
+	// 관리자 페이지 이동	
+	public ModelAndView goAdmin(HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		// 세션에서 mbType을 가져옴 --> 관리자 회원(mbType==3)인지 확인하기
+		if(session.getAttribute("mbType").toString().equals("3")){ //관리자라면
+			mav.setViewName("etc/admin"); // .jsp
+		}else { //관리자가 아니라면 홈화면으로
+			mav.setViewName("./"); // .jsp
+		}
+		return mav;
+	}
+	
+	// 통계 가져오기(ajax)	
 }
