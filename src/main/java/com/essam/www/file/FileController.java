f7fdcd58182e3bb53776a3d80b6e704c77453e55
hogeun9x <hogeun_9x@naver.com>
2021-04-22 15:42:25 +0900

    FileController - @RestController로 변경

--- src/main/java/com/essam/www/file/FileController.java
+++ src/main/java/com/essam/www/file/FileController.java
@@ -6,8 +6,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FileController {
 	@Autowired
 	private FileMM fm;
