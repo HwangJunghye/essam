a6cffc13e3ebd7322600c4626839ebfa04b6fad1
hogeun9x <hogeun_9x@naver.com>
2021-04-29 10:10:43 +0900

    Interceptor에 검증부분 추가(작업중)

--- src/main/java/com/essam/www/interceptor/CommonInterceptor.java
+++ src/main/java/com/essam/www/interceptor/CommonInterceptor.java
@@ -3,8 +3,11 @@ package com.essam.www.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.FlashMapManager;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.essam.www.bean.MemberBean;
 
@@ -15,8 +18,11 @@ public class CommonInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		String servletPath = request.getServletPath(); // 서블릿 주소
+		String indexPath = request.getContextPath() + "/"; // 인덱스 주소
+		
 		// System.out.println("preHandler Call : " + request.getServletPath());
-		log.info("preHandler Call : " + request.getServletPath());
+		log.info("preHandler Call : " + servletPath);
 		// 로그인 검증이나 권한 검증시 여기에 코드 추가
 
 		// 로그인 정보 가져오기
@@ -24,9 +30,65 @@ public class CommonInterceptor extends HandlerInterceptorAdapter {
 		if (mb == null) {
 			// 로그아웃 상태인 경우
 			log.info("user status : logout");
+			switch(servletPath) {
+			// 회원타입 검증에 있는 URL도 추가할 것.
+			// 로그인이 아니라면 
+			// 회원타입 검증이 필요한 모든 서비스도 사용 불가능하니까
+			case "/classjoin":
+			case "/mypage" :
+			case "/memberupdate" :
+			case "/teacher_profile" :
+			case "/teacher_profile/write" :
+			case "/teacher_profile/update" :
+			case "/teacher_profile/delete" :
+			case "/myclass_t" :
+			case "/myclass_s" :
+			// case "/class/classinfo" :
+			// case "/class/goboardwrite" :
+			// case "/class/boardwrite" :
+			// 어떤 url 추가해야 할지 정리 필요.
+			// 개발 단계이기 때문에 로그인이 반드시 필요한 항목 외에는 주석처리함
+			case "/logout" :
+			// 이 위에 case문 추가
+				FlashMap fMap = new FlashMap();
+				fMap.put("fMsg", "로그인이 필요합니다");
+				FlashMapManager fMapManager = RequestContextUtils.getFlashMapManager(request);
+				fMapManager.saveOutputFlashMap(fMap, request, response);
+				response.sendRedirect(indexPath);
+				return false;
+			}
 		} else {
 			// 로그인 상태인 경우
 			log.info("user status : login(" + mb.getMbId() + ")");
+			
+			// 회원타입에 따른 작업
+			if(mb.getMbType()==1) {
+				// 학생회원인 경우
+				switch(servletPath) {
+				case "/class/classinfo/update":
+				// 주소 추가 필요
+					FlashMap fMap = new FlashMap();
+					fMap.put("fMsg", "권한이 존재하지 않습니다");
+					FlashMapManager fMapManager = RequestContextUtils.getFlashMapManager(request);
+					fMapManager.saveOutputFlashMap(fMap, request, response);
+					response.sendRedirect(indexPath);
+					return false;
+				}
+			}else if (mb.getMbType()==2){
+				// 교사회원인 경우
+				switch(servletPath) {
+				case "/classjoin":
+				// 주소 추가 필요
+					FlashMap fMap = new FlashMap();
+					fMap.put("fMsg", "권한이 존재하지 않습니다");
+					FlashMapManager fMapManager = RequestContextUtils.getFlashMapManager(request);
+					fMapManager.saveOutputFlashMap(fMap, request, response);
+					response.sendRedirect(indexPath);
+					return false;
+				}
+			}else {
+				// 관리자인 경우
+			}
 		}
 
 		return true; // 다른 페이지로 이동할 경우 false 리턴
