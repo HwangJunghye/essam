203a63880b50b9294c763ecf1c0c78ef49faefbe
goyeonmi <yuri0113@naver.com>
2021-05-13 17:04:20 +0900

    재푸쉬

--- src/main/java/com/essam/www/common/CommonMM.java
+++ src/main/java/com/essam/www/common/CommonMM.java
@@ -206,7 +206,7 @@ public class CommonMM {
 			}
 			mav.addObject("classInfo", cb);
 			//mav에 네비타이틀 추가
-			mav.addObject("navtext", "클래스 > "+ cb.getClsName());
+			mav.addObject("navtext", "클래스 > "+ cb.getClsName()); 
 			
 			//강사 정보 가져와 mav에 담기 (MemberMM)
 			TeacherBean tb = mDao.getTeacherProfile(cb.getMbId());
